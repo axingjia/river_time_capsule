77d0aa5378c25dbbaea7340df81bf30d683277e0
axingjia <axingjia@gmail.com>
2018-08-22 17:53:47 -0700

    relentless song5

--- song_navi.php
+++ song_navi.php
@@ -1,5 +1,5 @@
 <style>
-	.songtitle1,.songtitle2,.songtitle3,.songtitle4{
+	.songtitle{
 		color: white;
 		font-size: 12px;
 	}
@@ -7,12 +7,14 @@
 <div style="position:absolute;left:5px;top:5px;">
 	<span class="glyphicon glyphicon-chevron-left" aria-hidden="true" style="color:white;font-size: 30px;cursor:pointer" onClick="window.location='./index.php';"></span>
 	<br><br>
-	<a  href="./songlist.php" class='songtitle1'>1.This is Amazing Grace</a>
+	<a  href="./songlist.php" class='songtitle1 songtitle'>1.This is Amazing Grace</a>
 	<br>
-	<a  href="./songlist2.php" class="songtitle2">2.Reckless Love</a>
+	<a  href="./songlist2.php" class="songtitle2 songtitle">2. Reckless Love</a>
 	<br>
-	<a  href="./songlist3.php" class="songtitle3">3. Great Are You Lord</a>
+	<a  href="./songlist3.php" class="songtitle3 songtitle">3. Great Are You Lord</a>
 	<br>
-	<a  href="./songlist4.php" class="songtitle4">4. This I believe</a>
+	<a  href="./songlist4.php" class="songtitle4 songtitle">4. This I believe</a>
+	<br>
+	<a  href="./songlist5.php" class="songtitle5 songtitle">5. Relentless</a>
 
 </div>
---
+++ songlist5.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>River Time Capsule</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body style="background-color:#3f4247;">
+		<?php include_once('./song_navi.php');?>
+		<div style="text-align:center;">
+			<div style="display:inline-block">
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/4N9mCUsTapY?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+			</div>
+			<div>
+				<div style="color:white;height:300px;overflow-y:scroll">
+					<h3>Lyrics</h3>
+					<div style="white-space: pre;">
+Salvation sounds a new beginning
+As distant hearts begin believing
+Redemption's bid is unrelenting
+Your love goes on
+Your love goes on
+You carry us, carry us
+When the world gives way
+You cover us, cover us
+With Your endless grace
+Your love is relentless
+Your love is relentless
+Your love is relentless
+Your love is relentless
+The time is up for chasing shadows
+You gave the world a light to follow
+A hope that shines beyond tomorrow
+Your love goes on
+Your love goes on
+You carry us, carry us
+When the world gives way
+You cover us, cover us
+With Your endless grace
+Your love is relentless
+Your love is relentless
+Your love is relentless
+Your love is relentless
+Tearing through the veil of darkness
+Breaking every chain, You set us free
+Fighting for the furthest heart You gave
+Your own life for all to see
+Tearing through the veil of darkness
+Breaking every chain, You set us free
+Fighting for the furthest heart You gave
+Your life (Your love is relentless)
+You carry us, carry us (Your love is relentless)
+You cover us, cover us (Your love is relentless)
+You carry us, carry us (Your love is relentless)
+You cover us, cover us (Your love is relentless)
+You carry us, carry us (Your love is relentless)
+You cover us, cover us (Your love is relentless)
+You carry us, carry us (Your love is relentless)
+You cover us, cover us (Your love is relentless)
+Your love is relentless
+Your love is relentless
+Your love is relentless
+Your love is relentless
+Songwriters: Joel Houston / Matt Crocker
+Relentless lyrics © Capitol Christian Music Group
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<script>
+			$(document).ready(function(){
+				$('.songtitle5').css('color',"#E7A977");
+			})
+		</script>
+		
+	</body>
+
+</html>
